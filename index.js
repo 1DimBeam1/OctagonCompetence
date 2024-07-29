d7301f32801c2aa8231f4eed6fb70d9db411768a
DimBeam <pro100dima_2016@mail.ru>
2024-07-30 01:32:21 +1000

    add in bot 3 commands

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new TelegramBot(token, {polling: true});
 const greetMessageUser = {};
 
 
-bot.onText(/\/*/, (msg) => 
+bot.onText(/\/start/, (msg) => 
 {
     const chatId = msg.chat.id;
     if (!greetMessageUser[chatId])
@@ -18,7 +18,27 @@ bot.onText(/\/*/, (msg) =>
         bot.sendMessage(chatId, message);
         greetMessageUser[chatId] = true;
     }
+    else
+    {
+        const message = "Введите команду /help";
+        bot.sendMessage(chatId, message);
+    }
 });
+
+bot.onText(/\/help/, (msg) => 
+{
+	let helpText = "Список команд:\n/site - отправляет в чат ссылку на сайт октагона\n/creator - отправляет в чат ФИО разработчика";
+    bot.sendMessage(msg.chat.id, helpText);
+});
+
+bot.onText(/\/site/, (msg) => {
+	bot.sendMessage(msg.chat.id, "https://forus.ru/");
+});
+
+bot.onText(/\/creator/, (msg) => {
+	bot.sendMessage(msg.chat.id, "Мачкасов Дмитрий Анатольевич");
+});
+
 
 
 bot.on('polling_error', (error) => 
